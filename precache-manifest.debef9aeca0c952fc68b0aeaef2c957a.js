b7f2deebd7bf5acc485dadc039e8030fc860b1fd
mdinfagro <mdinfagro@gmail.com>
2019-03-22 01:24:42 +0530

    Updates

---
+++ precache-manifest.debef9aeca0c952fc68b0aeaef2c957a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "af74f0e26a7b9a17afd895e18f74b92f",
+    "url": "/live_tv/static/media/loader.af74f0e2.gif"
+  },
+  {
+    "revision": "9a34e6508646c085377557eaec886656",
+    "url": "/live_tv/static/media/background.9a34e650.jpg"
+  },
+  {
+    "revision": "f867fb0dc724baa504119808e09fe71d",
+    "url": "/live_tv/static/media/avatar.f867fb0d.png"
+  },
+  {
+    "revision": "670d50a15035d62b89a1",
+    "url": "/live_tv/static/js/runtime~main.670d50a1.js"
+  },
+  {
+    "revision": "1ae4b4f82827b67fcacc",
+    "url": "/live_tv/static/js/main.1ae4b4f8.chunk.js"
+  },
+  {
+    "revision": "33904d11a89114bf5df2",
+    "url": "/live_tv/static/js/2.33904d11.chunk.js"
+  },
+  {
+    "revision": "1ae4b4f82827b67fcacc",
+    "url": "/live_tv/static/css/main.4297c72d.chunk.css"
+  },
+  {
+    "revision": "33904d11a89114bf5df2",
+    "url": "/live_tv/static/css/2.00e05589.chunk.css"
+  },
+  {
+    "revision": "d4c5437f5f68acccfb51243ec510d299",
+    "url": "/live_tv/index.html"
+  }
+];
